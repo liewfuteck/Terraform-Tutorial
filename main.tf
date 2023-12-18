436120f83c526ffab505afe817b00c2681a70849
liewfuteck <liewfuteck@gmail.com>
2023-12-18 17:05:06 +0800

    Deployed a simple server

---
+++ main.tf
@@ -0,0 +1,129 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+# Access key ID, secret access key and region have been stored via AWS CLI
+}
+
+# 1. Create VPC
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# 2. Create IGW
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "terraform_igw"
+  }
+}
+
+# 3. Create custom route table
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "terraform-prod-route-table"
+  }
+}
+
+# 4. Create a subnet where web server will reside on
+resource "aws_subnet" "subnet-1" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Prod subnet"
+  }
+}
+
+# 5. Associate subnet with route table
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.prod-route-table.id
+}
+
+# 6. Create security group to allow ports 22 (SSH), 80 (HTTP) and 443 (HTTPS)
+
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow web traffic"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "HTTP from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "HTTPS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_web"
+  }
+}
+
+# 7. Create network interface with an IP in the subnet created in Step 4
+
+resource "aws_network_interface" "web-server-nic" {
+  subnet_id       = aws_subnet.subnet-1.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_web.id]
+}
+
+# 8. Assign elastic IP to network interface created in Step 7
+resource "aws_eip" "bar" {
+  domain = "vpc"
+  associate_with_private_ip = "10.0.1.50"
+  depends_on                = [aws_internet_gateway.gw]
+}
+
+# 9. Create server for further use
+
+resource "aws_instance" "web-server-instance" {
+  ami           = "ami-0759f51a90924c166"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-1a"
+
+  network_interface {
+    network_interface_id = aws_network_interface.web-server-nic.id
+    device_index = 0
+  }
+}
